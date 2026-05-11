248f7dfa2f61a5668b2fb197dd3cced7ed351e0e
mishravishu-741 <mishravishu741@gmail.com>
2026-05-12 00:58:05 +0530

    Create leet881.java

---
+++ Array/Twopointer/oppositeend/leet881.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int n=people.length;
+        int left=0;
+        int right=n-1;
+        int count=0;
+        while(left<right){
+            if(people[left]+people[right]<=limit){
+                count++;
+                left++;
+                right--;
+
+            }else{
+                right--;
+                count++;
+            }
+            if (left == right) {
+            count++;
+        }
+        }
+        return count;
+
+
+        
+    }
+}
